ea33153869d2dd351b97e4db986c4287b9b8151a
karenmcewen <karenf_email@yahoo.com>
2017-10-16 21:12:26 -0400

    first commit

---
+++ TuringTechnical/Models/Course.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace TuringTechnical.Models
+{
+    public class Course
+    {
+        [Key]
+        public int ID { get; set; }
+        public string Title {get;set;}
+        public string Description { get; set; }
+        public int CreditHours { get; set; }
+        public int ClassMax { get; set; } //ClassMax=maximum number of students in class
+        public string ClassFormat { get; set; } //ClassFormat = online, in person, hybrid, self-study
+    }
+}
---
+++ TuringTechnical/Models/Student.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace TuringTechnical.Models
+{
+    public class Student
+    {
+        [Key]
+        public int ID { get; set; }
+        public string Name { get; set; }
+        //can change the format that is displayed by putting a [] above the definition
+        [DisplayFormat(DataFormatString = "{0:mm/dd/yyyy}")]
+        public DateTime BirthDate { get; set; }
+        [DisplayFormat(DataFormatString = "{0:mm/dd/yyyy}")]
+        public DateTime EnrollDate { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+
+    }
+}
